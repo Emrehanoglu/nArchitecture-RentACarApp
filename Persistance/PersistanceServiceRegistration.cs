ef35ba065efb7d0c4d2f31221515937611fb677b
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-04 22:23:01 +0300

    RentACarApp #11

--- Persistance/PersistanceServiceRegistration.cs
+++ Persistance/PersistanceServiceRegistration.cs
@@ -22,6 +22,7 @@ public static class PersistanceServiceRegistration
 		//useSqlServer dedikten sonra connectionString 'i verdim.
 		services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
 		services.AddScoped<IBrandRepository, BrandRepository>();
+		services.AddScoped<IModelRepository, ModelRepository>();
 		return services;
 	}
 }
