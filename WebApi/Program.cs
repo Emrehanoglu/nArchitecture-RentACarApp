cef6cf14ecf8cd321db0c6713252721313f38551
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-07 23:29:47 +0300

    RentACarApp #17

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -13,8 +13,8 @@ builder.Services.AddPersistanceServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
+ 
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
