210c3ed37281cb4924fba39f613d4709fea93045
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-02-08 04:17:43 +0300

    RentACarApp #18

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+//kullanıcıdan gelen istek CreateBrandCommand oldu. için onu validasyona sokacağım
+public class CreateBrandCommandValidator:AbstractValidator<CreateBrandCommand>
+{
+    //FluentValidation 'da validasyonlar parametresiz ctor içerisinde yazılır.
+    public CreateBrandCommandValidator()
+    {
+        RuleFor(c=>c.Name).NotEmpty().MinimumLength(2);
+    }
+}
